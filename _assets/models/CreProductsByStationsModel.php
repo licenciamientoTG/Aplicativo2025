93102d0be9bfa1d0574b09d2d984b4fe2bf1ffd9
amartinezTG <alejandro.martinez@totalgas.com>
2025-09-04 00:42:11 -0600

    aldo

--- _assets/models/CreProductsByStationsModel.php
+++ _assets/models/CreProductsByStationsModel.php
@@ -30,10 +30,8 @@ class CreProductsByStationsModel extends Model{
         return (bool)$this->sql->insert($query, [$controlGasStationId, $creProductId, $creSubProductId, $creSubProductBrandId]);
     }
 
-
     function getProductsByStations($codgas, $from) : array | false {
         $queryParts = []; // Arreglo para almacenar cada parte de la consulta
-
         foreach (explode(",", $codgas) as $key => $station) {
             $ip = trim($this->linked_server[intval($station)], '[]');
             // Vamos a verificar que haya conexion con el servidor de la estacion $this->linked_server[intval($station)]
@@ -45,12 +43,70 @@ class CreProductsByStationsModel extends Model{
                     $t5 = $t4 + 100;
                     $t6 = $t5 + 100;
                     $t7 = $t6 + 100;
-                    $queryParts[] = "SELECT t{$key}.*, t{$t2}.*, t{$t3}.nombre AS product, t{$t4}.nombre AS subProduct, t{$t5}.nombre AS subProductBrand, LTRIM(t{$t6}.den) AS controlGasProduct, t{$t7}.rfc FROM     OPENQUERY({$this->linked_server[intval($station)]}, 'SELECT t1.*, (t1.SaldoReal - t1.SaldoFinal) AS Merma, t2.abr COLLATE Latin1_General_CI_AI AS gasStation, t2.nropcc COLLATE Latin1_General_CI_AI AS numeroPermisoCRE FROM {$this->short_databases[intval($station)]}.[vw_ventas_resumen] t1 LEFT JOIN {$this->short_databases[intval($station)]}.Gasolineras t2 ON t1.codgas = t2.cod WHERE t1.fch = {$from}') AS t{$key} LEFT JOIN [devTotalGas].[dbo].[creProductsByStations] t{$t2} ON t{$key}.codgas = t{$t2}.controlGasStationId AND t{$key}.codprd = t{$t2}.controlGasProductId LEFT JOIN [devTotalGas].[dbo].[creProductos] t{$t3} ON t{$t2}.creProductId = t{$t3}.productoId LEFT JOIN [devTotalGas].[dbo].[creSubProductos] t{$t4} ON t{$t2}.creSubProductId = t{$t4}.subProductoId LEFT JOIN [devTotalGas].[dbo].[creTipoSubProductoMarca] t{$t5} ON t{$t2}.creSubProductBrandId = t{$t5}.tipoSubProductoMarcaId LEFT JOIN [SG12].[dbo].[Productos] t{$t6} ON t{$key}.codprd = t{$t6}.cod LEFT JOIN [TG].[dbo].[Estaciones] t{$t7} ON t{$key}.codgas = t{$t7}.Codigo"; // Agregar cada parte de la consulta
+                    $queryParts[] = "
+                        SELECT
+                            t{$key}.*,
+                            t{$t2}.*,
+                            t{$t3}.nombre AS product,
+                            t{$t4}.nombre AS subProduct,
+                            t{$t5}.nombre AS subProductBrand,
+                            LTRIM(t{$t6}.den) AS controlGasProduct,
+                            t{$t7}.rfc
+                        FROM
+                            OPENQUERY({$this->linked_server[intval($station)]}, '
+                                SELECT
+                                    v.*,
+                                    (v.SaldoReal - v.SaldoFinal) AS Merma,
+                                    g.abr COLLATE Latin1_General_CI_AI AS gasStation,
+                                    g.nropcc COLLATE Latin1_General_CI_AI AS numeroPermisoCRE
+                                FROM (
+                                    SELECT DISTINCT
+                                        codprd
+                                    FROM {$this->short_databases[intval($station)]}.[vw_ventas_resumen]
+                                ) p
+                                CROSS APPLY (
+                                    SELECT TOP (1)
+                                        x.*
+                                    FROM {$this->short_databases[intval($station)]}.[vw_ventas_resumen] x
+                                    WHERE x.codprd = p.codprd
+                                        AND x.fch <= (
+                                            SELECT
+                                                CASE
+                                                    WHEN EXISTS (
+                                                        SELECT 1
+                                                        FROM {$this->short_databases[intval($station)]}.[vw_ventas_resumen]
+                                                        WHERE fch = {$from}
+                                                    )
+                                                    THEN {$from}
+                                                    ELSE (
+                                                        SELECT MAX(fch)
+                                                        FROM {$this->short_databases[intval($station)]}.[vw_ventas_resumen]
+                                                    )
+                                                END
+                                        )
+                                    ORDER BY x.fch DESC
+                                ) v
+                                LEFT JOIN {$this->short_databases[intval($station)]}.Gasolineras g
+                                    ON v.codgas = g.cod
+                            ') AS t{$key}
+                            LEFT JOIN [devTotalGas].[dbo].[creProductsByStations] t{$t2}
+                                ON t{$key}.codgas = t{$t2}.controlGasStationId
+                                AND t{$key}.codprd = t{$t2}.controlGasProductId
+                            LEFT JOIN [devTotalGas].[dbo].[creProductos] t{$t3}
+                                ON t{$t2}.creProductId = t{$t3}.productoId
+                            LEFT JOIN [devTotalGas].[dbo].[creSubProductos] t{$t4}
+                                ON t{$t2}.creSubProductId = t{$t4}.subProductoId
+                            LEFT JOIN [devTotalGas].[dbo].[creTipoSubProductoMarca] t{$t5}
+                                ON t{$t2}.creSubProductBrandId = t{$t5}.tipoSubProductoMarcaId
+                            LEFT JOIN [SG12].[dbo].[Productos] t{$t6}
+                                ON t{$key}.codprd = t{$t6}.cod
+                            LEFT JOIN [TG].[dbo].[Estaciones] t{$t7}
+                                ON t{$key}.codgas = t{$t7}.Codigo
+                    ";
                 }
             }
         }
         $query = implode(" UNION ", $queryParts); // Unir todas las partes con UNION
-
         return $this->sql->select($query) ?: false;
     }
 }
