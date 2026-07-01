b2bd7e4011fe7fdd7f18d1bb0532c47aa4c1453c
aochoaTG <aldo.ochoa@totalgas.com>
2026-07-01 11:22:39 -0600

    Agregar URLs amigables para DataStudio (web.config + router.php)

--- router.php
+++ router.php
@@ -9,6 +9,14 @@ if ($path !== '/' && file_exists(__DIR__ . $path)) {
     return false;
 }
 
+// URLs amigables de DataStudio: /DataStudio/{version}/{report} -> DataStudio/index.php?version=&report=
+if (preg_match('#^/DataStudio/([^/]+)/([^/]+)$#', $path, $m)) {
+    $_GET['version'] = $m[1];
+    $_GET['report']  = $m[2];
+    require __DIR__ . '/DataStudio/index.php';
+    return;
+}
+
 // Simular lo que hacía web.config: index.php?url={R:1}
 $_GET['url'] = ltrim($path, '/'); // ejemplo: "/administration/doc_agujita" -> "administration/doc_agujita"
 
